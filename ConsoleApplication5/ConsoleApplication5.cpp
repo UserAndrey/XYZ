9ea6c220509963ab9ff053e38572dec1344fc0d5
User <User@WIN-A4TOTEJDR9Q>
2022-04-15 21:42:29 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main()
+{   setlocale(LC_ALL, "Russian");
+    bool T; T = true;
+    if (T)
+    {
+        cout << "Привет, мир\n";
+    }
+    else cout << "exit";
+    float x, y, z;
+    cout << "Input x:\n";
+    cin >> x;
+    cout << "Input y:\n";
+    cin >> y;
+    cout << "Input z:\n";
+    cin >> z;
+    cout << "x= "<<x<<";y= "<<y<<";z= "<<z<<endl;
+    return 0;
+    system("pause");
+}
